7a82dbc8eab00fb1c7c00e96bce6afd3fafc7fc2
beomsu <haperd13@naver.com>
2023-09-21 22:09:23 +0900

    확인 - 범수

--- app/src/main/java/com/han/total/Activity/MainActivity.java
+++ app/src/main/java/com/han/total/Activity/MainActivity.java
@@ -26,7 +26,7 @@ import com.han.total.Util.Logg;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-//천규 0920 10:30
+//천규 0920 10:30 범수 0921
 
 
 public class MainActivity extends AppCompatActivity {
